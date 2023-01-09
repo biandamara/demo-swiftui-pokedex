5d9bf069ecd3f6e84bd09c8ed686335e3f4d1ba4
Bian Damara <bian.damara@gmail.com>
2023-01-09 20:30:32 +0700

    Initial Commit

---
+++ Pokedex SwiftUI/Pokedex_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pokedex_SwiftUIApp.swift
+//  Pokedex SwiftUI
+//
+//  Created by Bian Damara on 09/01/23.
+//
+
+import SwiftUI
+
+@main
+struct Pokedex_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
